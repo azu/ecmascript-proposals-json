40effcb2f59570d5b523656195499e0e507724a3
azu <azuciao@gmail.com>
2019-02-02 20:47:22 +0900

    update travis

--- run.sh
+++ run.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-node ./get-proposal.js --date "2019-01-01"
 for i in {1..30}; do
   echo `gdate -I -d "2019-01-01 +$i days"`
   node ./get-proposal.js --date $(gdate -I -d "2019-01-01 +$i days");
