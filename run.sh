8fd0f7c79224a2a8a311fcd8d8c37595a1100cf2
azu <azuciao@gmail.com>
2019-04-06 12:12:14 +0900

    update data

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-for i in {0..90}; do
-  echo `gdate -I -d "2019-01-01 +$i days"`
-  node ./get-proposal.js --date $(gdate -I -d "2019-01-01 +$i days");
+for i in {0..365}; do
+  echo `gdate -I -d "2018-01-01 +$i days"`
+  node ./get-proposal.js --date $(gdate -I -d "2018-01-01 +$i days");
   sleep 1
 done
